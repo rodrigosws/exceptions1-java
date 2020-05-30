a3f8c798c00575cb19dd54c812e3985befb0939a
Rodrigo Pinto <rodrigosws@gmail.com>
2020-05-30 10:32:43 -0300

    Bad Solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,7 +10,6 @@ import model.entities.Reservation;
 
 public class Program {
 	
-	//Solucao muito ruim
 
 	public static void main(String[] args) throws ParseException {
 		
@@ -38,16 +37,14 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
+			String error = reservation.updateDates(checkIn, checkOut);
+			if(error != null ) {
+				System.out.println("Error in reservation: " + error);
 			}
-			else if (!checkOut.after(checkIn)){
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
+			else {
+				System.out.println("Reservation: " + reservation);
+				System.out.println();
 			}
-			
-			reservation.updateDates(checkIn, checkOut);
-			System.out.println("Reservation: " + reservation);
 		}
 	}
 
